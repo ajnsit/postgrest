0cb1a7b98d9b95c4587d453f54b1aeb70ba4bdc6
Anupam Jain <ajnsit@gmail.com>
2019-02-17 19:28:53 +0530

    Update Stackage version to latest. Remove all custom deps and cersions. Fold in ranged-sets, configurator-ng, and critbit.

---
+++ src/Data/Configurator/FromValue.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- |
+-- Module:      Data.Configurator.FromValue
+-- Copyright:   (c) 2016 Leon P Smith
+-- License:     BSD3
+-- Maintainer:  Leon P Smith <leon@melding-monads.com>
+
+module Data.Configurator.FromValue
+     ( MaybeParser
+     , runMaybeParser
+     , FromMaybeValue(..)
+     , optionalValue
+     , requiredValue
+     , ValueParser
+     , runValueParser
+     , FromValue(..)
+     , ListParser
+     , FromListValue(..)
+     , listValue
+     , listValue'
+     , listElem
+     , ConversionError(..)
+     , ConversionErrorWhy(..)
+     , defaultConversionError
+     -- * Assorted primitive value parsers
+     , boundedIntegerValue
+     , integralValue
+     , fractionalValue
+     , realFloatValue
+     , fixedValue
+     , scientificValue
+     , textValue
+     , charValue
+     , typeError
+     , valueError
+     , extraValuesError
+     , missingValueError
+     ) where
+
+import Data.Configurator.FromValue.Implementation
+import Data.Configurator.Types
